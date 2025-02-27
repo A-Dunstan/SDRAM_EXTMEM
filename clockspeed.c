a1c8eae8e2238c748d2fc1ea4da22d3c4b8b90c4
Andrew Dunstan <a_dunstan@hotmail.com>
2025-02-28 03:58:21 +1100

    500MHz MCU requires different voltage settings, replace the core clockspeed functions

---
+++ clockspeed.c
@@ -0,0 +1,229 @@
+#include <stdint.h>
+#include "imxrt.h"
+#include "wiring.h"
+#include "debug/printf.h"
+
+// A brief explanation of F_CPU_ACTUAL vs F_CPU
+//  https://forum.pjrc.com/threads/57236?p=212642&viewfull=1#post212642
+volatile uint32_t F_CPU_ACTUAL = 396000000;
+volatile uint32_t F_BUS_ACTUAL = 132000000;
+
+// Define these to increase the voltage when attempting overclocking
+// The frequency step is how quickly to increase voltage per frequency
+// The datasheet says 1600 is the absolute maximum voltage.  The hardware
+// can actually create up to 1575.  But 1300 is the recommended limit.
+//  (earlier versions of the datasheet said 1300 was the absolute max)
+#define OVERCLOCK_MAX_VOLT  1300
+
+
+uint32_t set_arm_clock(uint32_t frequency);
+
+#define LUT0(opcode, pads, operand) (FLEXSPI_LUT_INSTRUCTION((opcode), (pads), (operand)))
+#define LUT1(opcode, pads, operand) (LUT0(opcode, pads, operand) << 16)
+#define CMD_SDR          FLEXSPI_LUT_OPCODE_CMD_SDR
+#define ADDR_SDR         FLEXSPI_LUT_OPCODE_RADDR_SDR
+#define READ_SDR         FLEXSPI_LUT_OPCODE_READ_SDR
+#define DUMMY_SDR        FLEXSPI_LUT_OPCODE_DUMMY_SDR
+#define PINS1            FLEXSPI_LUT_NUM_PADS_1
+
+static uint32_t flash_read_security(void) {
+  uint32_t brand = 0;
+  // unlock LUT
+  FLEXSPI_LUTKEY = FLEXSPI_LUTKEY_VALUE;
+  FLEXSPI_LUTCR = FLEXSPI_LUTCR_UNLOCK;
+  // first read the flash ID
+  FLEXSPI_LUT60 = LUT0(CMD_SDR, PINS1, 0x9F) | LUT1(READ_SDR, PINS1, 3);
+  FLEXSPI_LUT61 = FLEXSPI_LUT62 = FLEXSPI_LUT63 = 0;
+  FLEXSPI_IPCR0 = 0;
+  FLEXSPI_IPCR1 = FLEXSPI_IPCR1_ISEQID(15);
+  FLEXSPI_IPRXFCR = FLEXSPI_IPRXFCR_CLRIPRXF; // clear read FIFO
+  FLEXSPI_INTR = FLEXSPI_INTR_IPCMDDONE;      // clear done interrupt
+  asm volatile("dmb");
+  FLEXSPI_IPCMD = FLEXSPI_IPCMD_TRG;
+  while ((FLEXSPI_INTR & FLEXSPI_INTR_IPCMDDONE) == 0);
+  if ((FLEXSPI_RFDR0 & 0xFFFFFF) == 0x1870EF) { // flash looks like a micromod
+    FLEXSPI_LUT60 = LUT0(CMD_SDR, PINS1, 0x48) | LUT1(ADDR_SDR, PINS1, 24);
+    FLEXSPI_LUT61 = LUT0(DUMMY_SDR, PINS1, 8) | LUT1(READ_SDR, PINS1, 4);
+    FLEXSPI_IPCR0 = 3 << 12; // security register 3
+    FLEXSPI_IPCR1 = FLEXSPI_IPCR1_ISEQID(15);
+    FLEXSPI_IPRXFCR = FLEXSPI_IPRXFCR_CLRIPRXF; // clear read FIFO
+    FLEXSPI_INTR = FLEXSPI_INTR_IPCMDDONE;      // clear done interrupt
+    asm volatile("dmb");
+    FLEXSPI_IPCMD = FLEXSPI_IPCMD_TRG;
+    while ((FLEXSPI_INTR & FLEXSPI_INTR_IPCMDDONE) == 0);
+    brand = FLEXSPI_RFDR0;
+  }
+  // relock LUT
+  FLEXSPI_LUTKEY = FLEXSPI_LUTKEY_VALUE;
+  FLEXSPI_LUTCR = FLEXSPI_LUTCR_LOCK;
+
+  return brand;
+}
+
+static uint32_t freq_to_voltage(uint32_t freq)
+{
+  if (freq <= 24000000)
+    return 950;
+
+  uint32_t v = 1150;
+  if (freq > 432000000) {
+    uint32_t brand = flash_read_security();
+    if ((brand & 0xFF) == 'D' && ((brand >> 8) & 0xFF) == 'B') {
+      v += ((freq - 432000000) / 25000000) * 25;
+    } else if (freq > 528000000) {
+      v = 1250;
+      if (freq > 600000000) {
+        v += ((freq - 600000000) / 28000000) * 25;
+      }
+    }
+    if (v > OVERCLOCK_MAX_VOLT) v = OVERCLOCK_MAX_VOLT;
+  }
+
+  return v;
+}
+
+
+// stuff needing wait handshake:
+//  CCM_CACRR  ARM_PODF
+//  CCM_CBCDR  PERIPH_CLK_SEL
+//  CCM_CBCMR  PERIPH2_CLK_SEL
+//  CCM_CBCDR  AHB_PODF
+//  CCM_CBCDR  SEMC_PODF
+
+uint32_t set_arm_clock(uint32_t frequency)
+{
+  uint32_t cbcdr = CCM_CBCDR; // pg 1021
+  uint32_t cbcmr = CCM_CBCMR; // pg 1023
+  uint32_t dcdc = DCDC_REG3;
+
+  uint32_t voltage = freq_to_voltage(frequency);
+
+  // if voltage needs to increase, do it before switch clock speed
+  CCM_CCGR6 |= CCM_CCGR6_DCDC(CCM_CCGR_ON);
+  if ((dcdc & DCDC_REG3_TRG_MASK) < DCDC_REG3_TRG((voltage - 800) / 25)) {
+    printf("Increasing voltage to %u mV\n", voltage);
+    dcdc &= ~DCDC_REG3_TRG_MASK;
+    dcdc |= DCDC_REG3_TRG((voltage - 800) / 25);
+    DCDC_REG3 = dcdc;
+    while (!(DCDC_REG0 & DCDC_REG0_STS_DC_OK)) ; // wait voltage settling
+  }
+
+  if (!(cbcdr & CCM_CBCDR_PERIPH_CLK_SEL)) {
+    printf("need to switch to alternate clock during reconfigure of ARM PLL\n");
+    const uint32_t need1s = CCM_ANALOG_PLL_USB1_ENABLE | CCM_ANALOG_PLL_USB1_POWER |
+      CCM_ANALOG_PLL_USB1_LOCK | CCM_ANALOG_PLL_USB1_EN_USB_CLKS;
+    uint32_t sel, div;
+    if ((CCM_ANALOG_PLL_USB1 & need1s) == need1s) {
+      printf("USB PLL is running, so we can use 120 MHz\n");
+      sel = 0;
+      div = 3; // divide down to 120 MHz, so IPG is ok even if IPG_PODF=0
+    } else {
+      printf("USB PLL is off, use 24 MHz crystal\n");
+      sel = 1;
+      div = 0;
+    }
+    if ((cbcdr & CCM_CBCDR_PERIPH_CLK2_PODF_MASK) != CCM_CBCDR_PERIPH_CLK2_PODF(div)) {
+      // PERIPH_CLK2 divider needs to be changed
+      cbcdr &= ~CCM_CBCDR_PERIPH_CLK2_PODF_MASK;
+      cbcdr |= CCM_CBCDR_PERIPH_CLK2_PODF(div);
+      CCM_CBCDR = cbcdr;
+    }
+    if ((cbcmr & CCM_CBCMR_PERIPH_CLK2_SEL_MASK) != CCM_CBCMR_PERIPH_CLK2_SEL(sel)) {
+      // PERIPH_CLK2 source select needs to be changed
+      cbcmr &= ~CCM_CBCMR_PERIPH_CLK2_SEL_MASK;
+      cbcmr |= CCM_CBCMR_PERIPH_CLK2_SEL(sel);
+      CCM_CBCMR = cbcmr;
+      while (CCM_CDHIPR & CCM_CDHIPR_PERIPH2_CLK_SEL_BUSY) ; // wait
+    }
+    // switch over to PERIPH_CLK2
+    cbcdr |= CCM_CBCDR_PERIPH_CLK_SEL;
+    CCM_CBCDR = cbcdr;
+    while (CCM_CDHIPR & CCM_CDHIPR_PERIPH_CLK_SEL_BUSY) ; // wait
+  } else {
+    printf("already running from PERIPH_CLK2, safe to mess with ARM PLL\n");
+  }
+
+  // TODO: check if PLL2 running, can 352, 396 or 528 can work? (no need for ARM PLL)
+
+  // DIV_SELECT: 54-108 = official range 648 to 1296 in 12 MHz steps
+  uint32_t div_arm = 1;
+  uint32_t div_ahb = 1;
+  while (frequency * div_arm * div_ahb < 648000000) {
+    if (div_arm < 8) {
+      div_arm = div_arm + 1;
+    } else {
+      if (div_ahb < 5) {
+        div_ahb = div_ahb + 1;
+        div_arm = 1;
+      } else {
+        break;
+      }
+    }
+  }
+  uint32_t mult = (frequency * div_arm * div_ahb + 6000000) / 12000000;
+  if (mult > 108) mult = 108;
+  if (mult < 54) mult = 54;
+  printf("Freq: 12 MHz * %u / %u / %u\n", mult, div_arm, div_ahb);
+  frequency = mult * 12000000 / div_arm / div_ahb;
+
+  printf("ARM PLL=%x\n", CCM_ANALOG_PLL_ARM);
+  const uint32_t arm_pll_mask = CCM_ANALOG_PLL_ARM_LOCK | CCM_ANALOG_PLL_ARM_BYPASS |
+    CCM_ANALOG_PLL_ARM_ENABLE | CCM_ANALOG_PLL_ARM_POWERDOWN |
+    CCM_ANALOG_PLL_ARM_DIV_SELECT_MASK;
+  if ((CCM_ANALOG_PLL_ARM & arm_pll_mask) != (CCM_ANALOG_PLL_ARM_LOCK
+    | CCM_ANALOG_PLL_ARM_ENABLE | CCM_ANALOG_PLL_ARM_DIV_SELECT(mult))) {
+    printf("ARM PLL needs reconfigure\n");
+    CCM_ANALOG_PLL_ARM = CCM_ANALOG_PLL_ARM_POWERDOWN;
+    // TODO: delay needed?
+    CCM_ANALOG_PLL_ARM = CCM_ANALOG_PLL_ARM_ENABLE
+      | CCM_ANALOG_PLL_ARM_DIV_SELECT(mult);
+    while (!(CCM_ANALOG_PLL_ARM & CCM_ANALOG_PLL_ARM_LOCK)) ; // wait for lock
+    printf("ARM PLL=%x\n", CCM_ANALOG_PLL_ARM);
+  } else {
+    printf("ARM PLL already running at required frequency\n");
+  }
+
+  if ((CCM_CACRR & CCM_CACRR_ARM_PODF_MASK) != (div_arm - 1)) {
+    CCM_CACRR = CCM_CACRR_ARM_PODF(div_arm - 1);
+    while (CCM_CDHIPR & CCM_CDHIPR_ARM_PODF_BUSY) ; // wait
+  }
+
+  if ((cbcdr & CCM_CBCDR_AHB_PODF_MASK) != CCM_CBCDR_AHB_PODF(div_ahb - 1)) {
+    cbcdr &= ~CCM_CBCDR_AHB_PODF_MASK;
+    cbcdr |= CCM_CBCDR_AHB_PODF(div_ahb - 1);
+    CCM_CBCDR = cbcdr;
+    while (CCM_CDHIPR & CCM_CDHIPR_AHB_PODF_BUSY); // wait
+  }
+
+  uint32_t div_ipg = (frequency + 149999999) / 150000000;
+  if (div_ipg > 4) div_ipg = 4;
+  if ((cbcdr & CCM_CBCDR_IPG_PODF_MASK) != (CCM_CBCDR_IPG_PODF(div_ipg - 1))) {
+    cbcdr &= ~CCM_CBCDR_IPG_PODF_MASK;
+    cbcdr |= CCM_CBCDR_IPG_PODF(div_ipg - 1);
+    // TODO: how to safely change IPG_PODF ??
+    CCM_CBCDR = cbcdr;
+  }
+
+  //cbcdr &= ~CCM_CBCDR_PERIPH_CLK_SEL;
+  //CCM_CBCDR = cbcdr;  // why does this not work at 24 MHz?
+  CCM_CBCDR &= ~CCM_CBCDR_PERIPH_CLK_SEL;
+  while (CCM_CDHIPR & CCM_CDHIPR_PERIPH_CLK_SEL_BUSY) ; // wait
+
+  F_CPU_ACTUAL = frequency;
+  F_BUS_ACTUAL = frequency / div_ipg;
+  scale_cpu_cycles_to_microseconds = 0xFFFFFFFFu / (uint32_t)(frequency / 1000000u);
+
+  printf("New Frequency: ARM=%u, IPG=%u\n", frequency, frequency / div_ipg);
+
+  // if voltage needs to decrease, do it after switch clock speed
+  if ((dcdc & DCDC_REG3_TRG_MASK) > DCDC_REG3_TRG((voltage - 800) / 25)) {
+    printf("Decreasing voltage to %u mV\n", voltage);
+    dcdc &= ~DCDC_REG3_TRG_MASK;
+    dcdc |= DCDC_REG3_TRG((voltage - 800) / 25);
+    DCDC_REG3 = dcdc;
+    while (!(DCDC_REG0 & DCDC_REG0_STS_DC_OK)) ; // wait voltage settling
+  }
+
+  return frequency;
+}
+
